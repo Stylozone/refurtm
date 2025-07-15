8670f8ba23b44e9326fdd67764f9fac52d0caeda
phaicom <reawpai.chun@yahoo.com>
2025-07-16 02:01:55 +0700

    feat: add nest backend

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,6 @@
+import { join } from 'node:path'
 import configs from '@phaicom/eslint-config'
+import globals from 'globals'
 
 export default
 configs(
@@ -7,6 +9,23 @@ configs(
     formatters: {
       html: true,
       css: true,
+    },
+  },
+  {
+    files: ['./apps/api/**/*.{js,ts}'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: join(import.meta.dirname, './apps/api'),
+      },
+    },
+    rules: {
+      'n/prefer-global/process': 'off',
     },
   },
 )
