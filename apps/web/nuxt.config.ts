65d2a38f98e0d0bf868010696051aa1edd864d73
phaicom <reawpai.chun@yahoo.com>
2025-07-21 04:37:57 +0700

    chore: set codegen graph ql for false in production

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,24 +1,25 @@
+/* eslint-disable node/prefer-global/process */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  devServer: {
+  'compatibilityDate': '2025-07-15',
+  'devtools': { enabled: true },
+  'devServer': {
     port: 3001,
   },
-  css: ['~/assets/css/main.css'],
-  vite: {
+  'css': ['~/assets/css/main.css'],
+  'vite': {
     plugins: [
       tailwindcss(),
     ],
   },
-  modules: ['@nuxt/image', '@nuxt/fonts', 'shadcn-nuxt', 'nuxt-graphql-client'],
-  shadcn: {
+  'modules': ['@nuxt/image', '@nuxt/fonts', 'shadcn-nuxt', 'nuxt-graphql-client'],
+  'shadcn': {
     prefix: '',
     componentDir: './app/components/ui',
   },
-  // 'graphql-client': {
-  //   codegen: false,
-  // },
+  'graphql-client': {
+    codegen: process.env.NODE_ENV !== 'production',
+  },
 })
